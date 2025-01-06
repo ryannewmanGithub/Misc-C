531a5719918638c3772e004793c3331df56cd1f8
Ryan Newman <RyanNewman280@gmail.com>
2025-01-06 09:48:25 -0800

    Printing \t:

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+    //int t = pow(5, 2);
+    //printf("%d\n", t);
+    printf("\\t");
+    printf("\n");
+    
+
+    return 0;
+    int c;
+
+    c = getchar();
+
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    }
+}
